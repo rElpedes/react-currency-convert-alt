ef43b1c63b5481ea9091b14c905dd6c264aa7a1a
Admin <admin@WorkstationC.local>
2022-04-28 21:07:34 +0930

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-export function App() {
-  return (
-    <div>
-      <h1>{new Date().toString()}</h1>
-      <p>blank webpack+react+babel template</p>
-    </div>
-  );
+export default function App() {
+	const [usdVar, setUsdVar] = useState(0);
+	const [audVar, setAudVar] = useState(0);
+
+	return (
+		<div>
+			<h1>USD/AUD Conversion</h1>
+			<div>
+				<span className="currLabel">US $</span>
+				<input
+					type="number"
+					className="usdInput"
+					value={usdVar}
+					onChange={(e) => {
+						setUsdVar(e.target.value);
+						setAudVar(e.target.value * 1.39);
+					}}
+				/>
+			</div>
+			<div className="equals">=</div>
+			<div>
+				<span className="currLabel">AU $</span>
+				<input
+					type="number"
+					className="audInput"
+					value={audVar}
+					onChange={(e) => {
+						setAudVar(e.target.value);
+						setUsdVar(e.target.value * 0.72);
+					}}
+				/>
+			</div>
+		</div>
+	);
 }
